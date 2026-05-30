7a3d56e0035eb1756259cfa1e36e1530e37f2fbc
comradedemafia <comradedemafia@gmail.com>
2026-05-30 06:40:29 +0300

    chore: downgrade composio-core to version 0.4.8
    feat: add migration script to drop unused tables, constraints, policies, and functions in Supabase schema
    
    feat: create seed script for initializing auth tables in Supabase database

---
+++ supabase/seed.sql
@@ -0,0 +1,61 @@
+SET session_replication_role = replica;
+
+--
+-- PostgreSQL database dump
+--
+
+-- \restrict gWSab1oIyDH3LRYHgmqxLTyF2MuKnyQRXNP0B1MUGhHubp2HStrapSc1Yz2LiE7
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: audit_log_entries; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: instances; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: refresh_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Name: refresh_tokens_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: supabase_auth_admin
+--
+
+SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+-- \unrestrict gWSab1oIyDH3LRYHgmqxLTyF2MuKnyQRXNP0B1MUGhHubp2HStrapSc1Yz2LiE7
+
+RESET ALL;
